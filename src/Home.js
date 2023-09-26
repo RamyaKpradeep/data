0fb802b3c13ab6a10194071b5367dc895d9ca4ff
RamyaKpradeep <ramyakumaresan2110@gmail.com>
2023-09-26 19:57:01 +0530

    modified bg

--- src/Home.js
+++ src/Home.js
@@ -4,17 +4,19 @@ import { Link } from 'react-router-dom'
 function Home() {
   return (
     <div>
+       <div className='background'>
         <div className='row'>
             <div className='col-12 col-sm-12 col-md-12 col-lg-12'>
-            <h2 className='pt-5 text-dark' style={{fontWeight:'800'}}>Udemy Course Registered Students</h2>
+            <h2 className='pt-5 text-white' style={{fontWeight:'800'}}>SoloLearn Course Registered Students</h2>
             <Link className='btn btn-success mb-2' to="/userList"  style={{fontSize:20}}>Registered students </Link>
 
-                <div className='background'>
-                
-                </div>
+               
+                  </div>
             </div>
 
         </div>
+        
+              
     </div>
   )
 }
