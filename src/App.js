ab233d02b67107d7b70ba7fc37482e158518b08c
RamyaKpradeep <ramyakumaresan2110@gmail.com>
2023-09-26 13:34:44 +0530

    fetchdata using api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useEffect, useState } from "react";
+import Axios from "axios";
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+
+const App = () => {
+  const [users,setUsers] =useState([]);
+  useEffect(() => {
+    Axios.get('https://jsonplaceholder.typicode.com/users').then(res => {
+setUsers(res.data)
+    })
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container style={{textAlign:  "center",
+    fontSize: 18
+    }}>
+      <Table striped bordered hover size="sm">
+      <thead>
+        <tr>
+          <th>S.NO</th>
+          <th>Name</th>
+          <th>UserName</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+       {users.map((user) => {
+    return<tr>
+    <td>{user.id}</td>
+    <td>{user.name}</td>
+    <td>{user.username}</td>
+    <td>{user.email}</td>
+  </tr>
+       })}
+      </tbody>
+    </Table>
+    </Container>
+  )
 }
-
-export default App;
+export default App
